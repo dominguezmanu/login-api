69bad9053fe8a4ff74eb47d891a7618fd1f52b36
Manuel Dominguez <manudomavi@gmail.com>
2026-08-29 17:22:27 -0600

    agregado para TiBD

--- src/db.js
+++ src/db.js
@@ -7,6 +7,11 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  ssl: {
+    minVersion: 'TLSv1.2'
+  },
+
   waitForConnections: true,
   connectionLimit: 10
 });
