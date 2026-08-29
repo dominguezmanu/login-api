23ce7909cd8cfacdaf4e5c62906bb8118c432cef
Manuel Dominguez <manudomavi@gmail.com>
2026-08-29 17:15:14 -0600

    agregado para render

--- src/server.js
+++ src/server.js
@@ -26,6 +26,11 @@ app.get('/', async (req, res) => {
     });
   }
 });
+app.get('/health', (req, res) => {
+  res.json({
+    status: 'ok'
+  });
+});
 
 // LOGIN
 app.post('/api/login', async (req, res) => {
@@ -98,6 +103,6 @@ app.post('/api/login', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`API corriendo en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`API corriendo en puerto ${PORT}`);
 });
